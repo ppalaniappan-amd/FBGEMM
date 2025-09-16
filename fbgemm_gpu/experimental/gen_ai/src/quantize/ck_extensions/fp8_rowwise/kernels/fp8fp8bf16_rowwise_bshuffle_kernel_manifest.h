27764618263e00d8b9cbd07c5d3c796f6666df8e
Prashanth Palaniappan <prashanth.palaniappan@amd.com>
2025-09-16 00:36:17 +0000

    minot indentation

--- fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fp8_rowwise/kernels/fp8fp8bf16_rowwise_bshuffle_kernel_manifest.h
+++ fbgemm_gpu/experimental/gen_ai/src/quantize/ck_extensions/fp8_rowwise/kernels/fp8fp8bf16_rowwise_bshuffle_kernel_manifest.h
@@ -8,9 +8,7 @@
 
 
 #include <cstdlib>
-
 #include <ATen/ATen.h>
-
 
 at::Tensor
 fp8fp8bf16_rowwise_bpreshuffle_256x12x64x256_16x16_1x1_16x16x1_16x16x1_1x16x1x16_4x4x1_1x1_interwave_v1(
@@ -20,7 +18,6 @@ fp8fp8bf16_rowwise_bpreshuffle_256x12x64x256_16x16_1x1_16x16x1_16x16x1_1x16x1x16
     at::Tensor w_scale,
     at::Tensor Y);
 
-
 at::Tensor
 fp8fp8bf16_rowwise_bpreshuffle_256x16x128x256_16x16_1x2_16x16x1_16x16x1_1x16x1x16_8x8x1_1x2_intrawave_v1(
     at::Tensor XQ,
@@ -29,7 +26,6 @@ fp8fp8bf16_rowwise_bpreshuffle_256x16x128x256_16x16_1x2_16x16x1_16x16x1_1x16x1x1
     at::Tensor w_scale,
     at::Tensor Y);
 
-
 at::Tensor
 fp8fp8bf16_rowwise_bpreshuffle_256x16x64x256_16x16_1x1_16x16x1_16x16x1_1x16x1x16_4x4x1_1x1_intrawave_v1(
     at::Tensor XQ,
@@ -37,7 +33,6 @@ fp8fp8bf16_rowwise_bpreshuffle_256x16x64x256_16x16_1x1_16x16x1_16x16x1_1x16x1x16
     at::Tensor x_scale,
     at::Tensor w_scale,
     at::Tensor Y);
-
 
 at::Tensor
 fp8fp8bf16_rowwise_bpreshuffle_128x16x32x128_16x16_1x1_8x16x1_8x16x1_1x16x1x8_4x4x1_1x1_intrawave_v1(
